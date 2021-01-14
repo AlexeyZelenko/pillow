15c3eac381f109dce39f23e35e0a5727ff469327
AlexeyZelenko <zelenkooleksii75@gmail.com>
2021-01-14 13:12:14 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+<!--  <img alt="Vue logo" src="~/assets/logo.png" />-->
+	<div class="card mb-3">
+		<img
+				src="src/assets/pirjya.jpg"
+				class="card-img-top"
+				alt="..."
+		>
+		<div class="card-body" style="background-color: cornflowerblue">
+<!--			<h5 class="card-title">Card title</h5>-->
+<!--			<p class="card-text">Реставрація подушок, ковдр, перин.</p>-->
+<!--			<p class="card-text">Пошиття постільної білизни.</p>-->
+			<p class="text-white">Телефони: 0966936001, 0633653655 , 0677045632</p>
+			<p class="text-white-50">Вайбер: 0966936001</p>
+		</div>
+	</div>
+
+  <HelloWorld msg="Наші послуги" />
+
+	<footer>
+		<div>
+			<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d733.6029174975548!2d32.10251774294741!3d49.419197637518145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDnCsDI1JzEwLjIiTiAzMsKwMDYnMTIuOCJF!5e0!3m2!1sru!2sua!4v1610618716700!5m2!1sru!2sua" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+			<div>
+				<div class="brand">{{ new Date().getFullYear() }} — Галина Філатова</div>
+			</div>
+		</div>
+	</footer>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+	iframe {
+		width: 100%;
+		height: 640px;
+	}
+	/* Footer */
+	footer {
+		clear: both;
+		padding-top: 50px;
+		padding-bottom: 40px;
+		text-align: center;
+		background-color: #38404b;
+		margin-top: 30px;
+	}
+
+	footer .brand {
+		color: #bcc9dd;
+	}
+
+	footer .menu li {
+		padding-left: 10px;
+		padding-right: 10px;
+	}
+
+	footer .menu li a {
+		color: #bcc9dd;
+	}
+
+	footer .menu {
+		display: inline-block;
+		margin-top: 30px;
+		margin-bottom: 15px;
+	}
+
+	footer .copyright {
+		font-size: 12px;
+		color: #eee;
+	}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,198 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+	<!-- Modal -->
+	<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Реставрація подушок.</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<h3>
+						70*70-120грн
+					</h3>
+					<h3>70*50-100грн</h3>
+					<h3>60*60-100грн</h3>
+					<h3>50*50-90грн</h3>
+					<h3>40*60-90грн</h3>
+					<h3>40*40-80грн</h3>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Вийти</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Реставрація ковдр.</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<h3>Односпальне-1,40*2-800грн</h3>
+					<h3>Полуторне-1,50*2-900</h3>
+					<h3>Двоспальне 1,75*2-1000</h3>
+					<h3>Євро 2,1*2,2 -1200</h3>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Вийти</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Реставрація перин.</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<h3>від 500грн до 1000 в залежності від розміру.</h3>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Вийти</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="modal fade" id="exampleModal4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">Пошиття постільної білизни</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<h4>Постільна білизна від 700 грн до 1200 залежить
+						від розміру і гатунку тканини.</h4>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Вийти</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div
+			class="container"
+	>
+		<div class="row row-cols-1 row-cols-md-2 g-4">
+			<div class="col">
+				<div class="card">
+					<img
+							src="src/assets/pillow.png"
+							class="card-img-top"
+							alt="pillow"
+					>
+					<div class="card-body">
+						<h3 class="card-title">Реставрація подушок.</h3>
+					</div>
+					<!-- Button trigger modal -->
+					<button
+							type="button"
+							class="btn btn-primary"
+							data-bs-toggle="modal"
+							data-bs-target="#exampleModal"
+
+					>
+						Більше
+					</button>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<img
+							src="src/assets/carpet.png"
+							class="card-img-top"
+							alt="carpet"
+					>
+					<div class="card-body">
+						<h3 class="card-title">Реставрація ковдр.</h3>
+					</div>
+					<!-- Button trigger modal -->
+					<button
+							type="button"
+							class="btn btn-primary"
+							data-bs-toggle="modal"
+							data-bs-target="#exampleModal2"
+
+					>
+						Більше
+					</button>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card h-100">
+					<img
+							src="src/assets/feather_bed.png"
+							class="card-img-top"
+							alt="feather bed"
+					>
+					<div class="card-body">
+						<h3 class="card-title">Реставрація перин.</h3>
+					</div>
+					<!-- Button trigger modal -->
+					<button
+							type="button"
+							class="btn btn-primary"
+							data-bs-toggle="modal"
+							data-bs-target="#exampleModal3"
+
+					>
+						Більше
+					</button>
+				</div>
+			</div>
+			<div class="col">
+				<div class="card">
+					<img
+							src="src/assets/postal.jpg"
+							class="card-img-top"
+							alt="postal"
+					>
+					<div class="card-body">
+						<h3 class="card-title">Пошиття постільної білизни</h3>
+					</div>
+					<!-- Button trigger modal -->
+					<button
+							type="button"
+							class="btn btn-primary"
+							data-bs-toggle="modal"
+							data-bs-target="#exampleModal4"
+
+					>
+						Більше
+					</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0,
+			services: [
+					{
+					    img: '',
+							alt: '',
+							title: 'Card title',
+							text: ''
+					}
+			]
+    }
+  }
+}
+</script>
